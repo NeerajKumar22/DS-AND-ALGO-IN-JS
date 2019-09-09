d09db3d66c66f0256bfc6ced29264d93d830fefd
NeerajKumar22 <ineerajkr@gmail.com>
2019-09-09 23:14:17 +0530

    about queue methods peint, enqueue, dequeue, front, size, isEmpty

---
+++ queue/queue.js
@@ -0,0 +1,32 @@
+// Queues (FIFO) first-in-first-out
+
+function Queue() {
+  collection = [];
+  this.print = function() {
+    console.log(collection);
+  };
+  this.enqueue = function(element) {
+    collection.push(element);
+  };
+  this.dequeue = function() {
+    return collection.shift();
+  };
+  this.front = function() {
+    return collection[0];
+  };
+  this.size = function() {
+    return collection.length();
+  };
+  this.isEmpty = function() {
+    return collection.length === 0;
+  };
+}
+
+var q = new Queue();
+q.enqueue("a");
+q.enqueue("b");
+q.enqueue("c");
+q.print(); // ['a', 'b', 'c']
+console.log(q.dequeue()); // a
+console.log(q.front()); // b
+q.print(); // ['b', 'c']
