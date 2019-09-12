3fd16ba6ac789f94d6bbff17e9c86425cb0bae5f
NeerajKumar22 <ineerajkr@gmail.com>
2019-09-12 14:30:26 +0530

    Link list operations e.g. add, remove, size, indexOf, addAt, removeAt

---
+++ linkedList/linkedList.js
@@ -0,0 +1,133 @@
+// Linked List
+
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function(element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function() {
+    return length;
+  };
+
+  this.head = function() {
+    return head;
+  };
+
+  this.add = function(element) {
+    var node = new Node(element);
+    if (head === null) {
+      head = node;
+    } else {
+      currentNode = head;
+
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+    length++;
+  };
+
+  this.remove = function(element) {
+    var currentNode = head;
+    var previousNode;
+    if (currentNode.element === element) {
+      head = currentNode.next;
+    } else {
+      while (currentNode.element !== element) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+  };
+
+  this.isEmpty = function() {
+    return length === 0;
+  };
+
+  this.indexOf = function(element) {
+    var currentNode = head;
+    var index = -1;
+
+    while (currentNode) {
+      index++;
+      if (currentNode.element === element) {
+        return index;
+      }
+      currentNode = currentNode.next;
+    }
+    return -1; // i.e element is not in the linked list
+  };
+
+  this.elementAt = function(index) {
+    var currentNode = head;
+    var count = 0;
+    while (count < index) {
+      count++;
+      currentNode = currentNode.next;
+    }
+    return currentNode.element;
+  };
+
+  this.addAt = function(index, element) {
+    var node = new Node(element);
+
+    var currentNode = head;
+    var previousNode;
+    var currentIndex = 0;
+
+    if (index > length) {
+      node.next = currentNode;
+      head = node;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      node.next = currentNode;
+      previousNode.next = node;
+    }
+    length++;
+  };
+
+  this.removeAt = function(index) {
+    var currentNode = head;
+    var previousNode;
+    var currentIndex = 0;
+
+    if (index < 0 || index >= length) {
+      return null;
+    }
+    if (index === 0) {
+      head = currentNode.next;
+    } else {
+      while (currentIndex < index) {
+        currentIndex++;
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next = currentNode.next;
+    }
+    length--;
+    return currentNode.element;
+  };
+}
+
+var conga = new LinkedList();
+conga.add("Kitten");
+conga.add("Puppy");
+conga.add("Dog");
+conga.add("Cat");
+conga.add("Fish");
+console.log(conga.size()); // 5
+console.log(conga.removeAt(3)); // "Cat"
+console.log(conga.elementAt(3)); // "Fish"
+console.log(conga.indexOf("Puppy")); // 1
+console.log(conga.size()); // 4
